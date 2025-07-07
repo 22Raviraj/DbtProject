b0afa1dbca6cfaab3e4c40ed741a5b5fe3aacd6a
Raviraj A <araviraj77@gmail.com>
2025-07-07 14:11:55 +0000

    test Stg_test_claim

--- models/staging/stg_test_claim.sql
+++ models/staging/stg_test_claim.sql
@@ -1,6 +1,6 @@
 {{ config(
   materialized='table',
-  post_hook=["{{ log_issue('stg_claims', 'failure', 'Staging completed') }}"]
+  post_hook=["{{ log_issue('stg_claims', 'Successful', 'Staging completed') }}"]
 ) }}
 
 SELECT * FROM {{ ref('raw_claims') }}  
